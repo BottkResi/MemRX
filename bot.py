9dba4eeb0ccfe15ecba15f0a5402d0020d292123
BottkResi <bottk.resi@gmail.com>
2025-03-20 01:43:12 +0000

    new file:   Procfile new file:   bot.py new file:   requirements.txt new file:   runtime.txt

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
+
+# Lee el TOKEN desde las variables de entorno
+TOKEN = os.environ.get("BOT_TOKEN")
+if not TOKEN:
+    raise Exception("No se encontró la variable BOT_TOKEN")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.chat.id
+    nombre = update.message.chat.first_name
+    mensaje = f"👋 ¡Hola, {nombre}! Bienvenido al bot de Diagnóstico por Imágenes. Usa /jugar para comenzar."
+    update.message.reply_text(mensaje)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    logger.info("Bot iniciado...")
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
